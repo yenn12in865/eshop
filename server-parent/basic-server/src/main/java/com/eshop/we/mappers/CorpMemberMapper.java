48aadbb2ba080cccacaeac59c23804e7c772de47
yenn12in865 <liulian1985422@126.com>
2021-06-07 21:58:50 +0800

    mapper

---
+++ server-parent/basic-server/src/main/java/com/eshop/we/mappers/CorpMemberMapper.java
@@ -0,0 +1,16 @@
+package com.eshop.we.mappers;
+
+import com.eshop.we.mapper.SuperMapper;
+import com.eshop.we.user.entity.CorpMember;
+
+/**
+ * <p>
+ * 社团成员 Mapper 接口
+ * </p>
+ *
+ * @author anonymity
+ * @since 2018-07-04
+ */
+public interface CorpMemberMapper extends SuperMapper<CorpMember> {
+
+}
---
+++ server-parent/basic-server/src/main/java/com/eshop/we/mappers/CorporationMapper.java
@@ -0,0 +1,17 @@
+package com.eshop.we.mappers;
+
+
+import com.eshop.we.mapper.SuperMapper;
+import com.eshop.we.user.entity.Corporation;
+
+/**
+ * <p>
+ * 社团 Mapper 接口
+ * </p>
+ *
+ * @author anonymity
+ * @since 2018-07-04
+ */
+public interface CorporationMapper extends SuperMapper<Corporation> {
+
+}
